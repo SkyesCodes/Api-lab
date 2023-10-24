49a02a976ba4f2eaff3f2030f0dc1b783698b0b1
Skye <Skye@supermind.us>
2023-10-24 12:24:01 -0400

    finished lab

---
+++ Api-lab/routes/cats.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+const catsCtrl = require('../controllers/cats')
+
+/* GET users listing. */
+router.get('/fact', catsCtrl.fact);
+
+module.exports = router;
